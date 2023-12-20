6babd3740c0ecee631082919f0b94c9d283e206e
Ngoc Tran <tbngoc21@clc.fitus.edu.vn>
2023-12-21 01:16:55 +0700

    locomotive scroll

--- src/duong_frontend/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import '../components/button/buttons.css'; 
-import '../components/common.css'; 
-import '../components/navbar/navbar.css';
-import '../components/form/form.css';
-import LocomotiveScroll from 'locomotive-scroll';
-
-const locomotiveScroll = new LocomotiveScroll();
