22cf1a4e8be476e6620a9473a241d1684e41611b
Ngoc Tran <tbngoc21@clc.fitus.edu.vn>
2023-12-21 02:46:07 +0700

    change font, add homepage

---
+++ src/duong_frontend/src/components/index.js
@@ -0,0 +1,9 @@
+/* Import styles */
+import "common.css";
+
+/* Import locomotive-scroll */
+import "locomotive-scroll/bundled/locomotive-scroll.css";
+import LocomotiveScroll from "locomotive-scroll";
+
+/* Locomotive scroll instance */
+const locomotiveScroll = new LocomotiveScroll();
