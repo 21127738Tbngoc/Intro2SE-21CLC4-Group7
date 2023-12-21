9ec198382946f3d9c5bab7cc717dd925c1ad7ea2
Ngoc Tran <tbngoc21@clc.fitus.edu.vn>
2023-12-21 14:36:08 +0700

    move form.css to common.css

--- src/duong_frontend/src/components/searchbar/searchbar.jsx
+++ src/duong_frontend/src/components/searchbar/searchbar.jsx
@@ -23,16 +23,10 @@ const Searchbar = () => {
                     <div className="d-flex flex-row g-4 d-flex">
                         <div className="input-group">
                             <div className="input-group-text">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                     fill="none">
-                                    <path
-                                        d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
-                                        stroke="#46692A" strokeWidth="2" strokeLinecap="round"
-                                        strokeLinejoin="round"/>
-                                </svg>
+                                <img src="/imgs/navbar/search-icon.svg" alt="Search"/>
                             </div>
                             <input type="text" className="form-control out" id="searchtext"
-                                   placeholder="Typing something"
+                                   placeholder="What are you searching for?"
                                    style={searchTextStyle}/>
 
                             <div className="input-group-text quit-search">
