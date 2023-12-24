470b7c415eed28d6abecc18cfbf9f93a95d98bee
Ngoc Tran <tbngoc21@clc.fitus.edu.vn>
2023-12-25 01:13:15 +0700

    remove card light, keep dark

--- src/duong_frontend/src/components/card/card.jsx
+++ src/duong_frontend/src/components/card/card.jsx
@@ -6,31 +6,29 @@ import addToCartHandler from '../Utils/addToCart';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle"
 
-
 function ProductCard(props) {
   return (
-    <div className="col">
-        {/* Product container  */}
-        <div className="product-ctn">
-          {/* Product img */}
-          <div className="product-img">
-          <img src={props.img} alt="product-img"></img>
-          </div>
-          {/* Product info */}
-          <div className="product-info">
-            <h6>{props.name}</h6>
-            <p className="p3 product-cat">{props.categories}</p>
-            <p className="title2 py-1 product-price">{props.price}</p>
-            <img className="rating-star" src="/imgs/card/rating-4.png" />
-          </div>
-        </div>
+          <div className="col">
+            <div className="product-ctn">
+              {/* Product img */}
+              <div className="product-img" >
+              <img src={props.img} alt="product-img"></img>
+              </div>
+              {/* Product info */}
+              <div className="product-info">
+                <h6>{props.name}</h6>
+                <p className="p3 product-cat">{props.categories}</p>
+                <p className="title2 py-1 product-price">{props.price}</p>
+                <img className="rating-star" src="/imgs/card/rating-4.png" />
+              </div>
+            </div>
 
-        {/* Product buttons */}
-        <div className="product-buttons justify-content-between">
-          <div onClick = {addToCartHandler} className="product-btn button2 no-right-border">ADD TO CART</div>
-          <a href="#" className="product-btn button2">VIEW PRODUCT</a>
-        </div>
-  </div>
+            {/* Product buttons */}
+            <div className="product-buttons justify-content-between" style={{width:"100%"}}>
+              <div onClick = {addToCartHandler} className="product-btn button2 no-right-border">ADD TO CART</div>
+              <a href="#" className="product-btn button2">VIEW PRODUCT</a>
+            </div>
+          </div>
   );
 }
 
