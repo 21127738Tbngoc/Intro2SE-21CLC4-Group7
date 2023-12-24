dd5f1472f2f661dcf44d40c4d5ce89e2ba3bd15a
Ngoc Tran <tbngoc21@clc.fitus.edu.vn>
2023-12-25 00:58:31 +0700

    add product-img style to product card

--- src/duong_frontend/src/components/card/card.jsx
+++ src/duong_frontend/src/components/card/card.jsx
@@ -9,27 +9,27 @@ import "bootstrap/dist/js/bootstrap.bundle"
 
 function ProductCardLight(props) {
   return (
-    <div className="product-ctn-l">
-      {/* Product img */}
-      <div className="product-img">
-      <img src={props.img} alt="product-img" style={{width:"100%", height:"15em"}} ></img>
-      </div>
-      {/* Product info */}
-      <div className="product-info">
-        <h6 style={{height:"3em"}}>{props.name}</h6>
-        <p className="p3 product-cat">{props.categories}</p>
-        <p className="title2 py-1 product-price">{props.price}</p>
-        <img className="rating-star" src="/imgs/card/rating-4.png" />
-      </div>
+    <div className="col">
+        {/* Product container  */}
+        <div className="product-ctn-l">
+          {/* Product img */}
+          <div className="product-img">
+          <img src={props.img} alt="product-img"></img>
+          </div>
+          {/* Product info */}
+          <div className="product-info">
+            <h6>{props.name}</h6>
+            <p className="p3 product-cat">{props.categories}</p>
+            <p className="title2 py-1 product-price">{props.price}</p>
+            <img className="rating-star" src="/imgs/card/rating-4.png" />
+          </div>
+        </div>
 
-    </div>
-
-    {/* Product buttons */}
-    <div className="product-buttons-d justify-content-between" style={{width:"100%"}}>
-      <div onClick = {addToCartHandler} className="product-btn-d button2 no-right-border">ADD TO CART</div>
-      <a href="#" className="product-btn-d button2">VIEW PRODUCT</a>
-    </div>
-
+        {/* Product buttons */}
+        <div className="product-buttons-d justify-content-between">
+          <div onClick = {addToCartHandler} className="product-btn-d button2 no-right-border">ADD TO CART</div>
+          <a href="#" className="product-btn-d button2">VIEW PRODUCT</a>
+        </div>
   </div>
   );
 }
@@ -58,7 +58,6 @@ function ProductCardDark(props) {
               <div onClick = {addToCartHandler} className="product-btn-d button2 no-right-border">ADD TO CART</div>
               <a href="#" className="product-btn-d button2">VIEW PRODUCT</a>
             </div>
-
           </div>
   );
 }
