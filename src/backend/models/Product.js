2f8918faeed8c06de1b1a160da1313c1cab1f6a1
Nguyễn Hữu Khánh <nhkhanh21@clc.fitus.edu.vn>
2023-12-30 03:57:14 +0700

    load data to shop

--- src/backend/models/Product.js
+++ src/backend/models/Product.js
@@ -2,26 +2,30 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true },
-    desc: { type: String, required: true, },
-    price: { type: Number, required: true },
-    tags: {type: Array},
-    img: { type: Array, required: true },
-    categories: { type: Array },
-    brand: {type: String},
-    qty: { type: Number },
-    color: { type: String },
-    pattern: {type: String},
-    feedback: [
-            {
-                user:{type: Object},
-                order: {type: Object},
-                rating:{type: Number},
-                content: {type: String}
-            },
-        ],
+      name: {type: String, required: true, unique: true},
+      room: {type: String},
+      category: {type: String},
+      thumbnail: {type: String},
+      price: {type: Number, required: true},
+      sku: {type: String},
+      images: {type: Array, required: true},
+      description: {type: String, required: true,},
+      quantity: {type: Number},
+      measurement: {type: String},
+      measure_img: {type: String},
+      pattern: {type: String},
+      tag: {type: Array},
+      feedback: [
+          {
+              user: {type: Object},
+              order: {type: Object},
+              rating: {type: Number},
+              content: {type: String}
+          },
+      ],
   },
-  { timestamps: true }
+  {timestamps: true}
 );
+
 
 module.exports = mongoose.model("Product", ProductSchema);
