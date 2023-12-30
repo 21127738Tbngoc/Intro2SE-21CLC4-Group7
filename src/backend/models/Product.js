37f505fb660b4d4ea235a2fe213f6bdedfeb689e
Nguyễn Hữu Khánh <nhkhanh21@clc.fitus.edu.vn>
2023-12-30 13:03:18 +0700

    restore products model

--- src/backend/models/Product.js
+++ src/backend/models/Product.js
@@ -2,29 +2,31 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
   {
-      name: {type: String, required: true, unique: true},
-      room: {type: String},
-      category: {type: String},
-      thumbnail: {type: String},
-      price: {type: Number, required: true},
+      name: { type: String, required: true, unique: true },
       sku: {type: String},
-      images: {type: Array, required: true},
-      description: {type: String, required: true,},
-      quantity: {type: Number},
+      room: {type: String},
       measurement: {type: String},
       measure_img: {type: String},
+      desc: { type: String, required: true, },
+      thumbnail: {type: String},
+      price: { type: Number, required: true },
+      tags: {type: Array},
+      img: { type: Array, required: true },
+      categories: { type: Array },
+      brand: {type: String},
+      qty: { type: Number },
+      color: { type: String },
       pattern: {type: String},
-      tag: {type: Array},
       feedback: [
           {
-              user: {type: Object},
+              user:{type: Object},
               order: {type: Object},
-              rating: {type: Number},
+              rating:{type: Number},
               content: {type: String}
           },
       ],
   },
-  {timestamps: true}
+    { timestamps: true }
 );
 
 
