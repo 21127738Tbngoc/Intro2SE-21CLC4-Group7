4fd3ac17c8b789fcd1467be23eeab90f2ef5aa48
Ngoc Tran <tbngoc21@clc.fitus.edu.vn>
2024-01-09 01:40:39 +0700

    done product detail

--- src/duong_frontend/src/pages/ProductDetail.jsx
+++ src/duong_frontend/src/pages/ProductDetail.jsx
@@ -15,14 +15,13 @@ import '../components/styles/ProductDetail.css';
 
 const ProductDetail = ({ Product }) => {
     const { addToCart } = useContext(ShopContext);
-
     const { productId } = useParams();
-
     const [quantity, setQuantity] = useState(1);
+    const [product, setProduct] = useState(0);
 
     const changeThumbnail = (event) => {
         const newSrc = event.target.src;
-        const thumbnail = document.querySelector('.product-thumbnail');
+        const thumbnail = document.querySelector('.product-detail-main');
         const images = document.querySelectorAll('.product-img-list img');
     
         thumbnail.src = newSrc;
@@ -36,18 +35,15 @@ const ProductDetail = ({ Product }) => {
         });
     };
     
-
     const minusQuantity = () => {
         if (quantity > 1) {
-            setQuantity(quantity - 1);
+        setQuantity(quantity - 1);
         }
     };
 
     const plusQuantity = () => {
         setQuantity(quantity + 1);
     };
-
-    const [product, setProduct] = useState(0);
 
     useEffect(() => {
         const fetchProduct = async () => {
@@ -77,41 +73,62 @@ const ProductDetail = ({ Product }) => {
             <div className="container-fluid">
                 <div className="container product-display">
                     <div className="row g-4">
-                        <div className="col">
+                        <div className="col left-display">
                             {/* Images */}
-                            <div className="left-display">
+                            
                                 <div className="product-img-list">
                                     <img src={product.img[0]} onClick={changeThumbnail} alt='haha' />
                                     <img src={product.img[2]} onClick={changeThumbnail} alt='haha'/>
                                     <img src={product.img[3]} onClick={changeThumbnail} alt='haha' />
                                     <img src={product.img[4]} onClick={changeThumbnail} alt='haha'/>
-
                                 </div>
                                 <img src={product.thumbnail} className="product-detail-main" />
-                            </div>
+                            
                         </div>
-                        <div className="col">
-                            <div className="right-display">
+                        <div className="col right-display" style={{ paddingLeft: '48px'}}>
+                            
                                 {/* Room & Categories */}
-                                <p className="button1 product-category">
-                                    {product.room} • {product.categories && product.categories.join(" & ")}
+                                <p className="button1 product-category" style={{ textAlign: 'left', color: 'var(--scheme-on-surface-variant)' }}>
+                                    {product.room.toUpperCase()} • {product.categories && product.categories.map(category => category.toUpperCase()).join(" & ")}
                                 </p>
                                 {/* Name */}
-                                <d4>{product.name}</d4>
+                                <d4 style={{ margin: '8px 0 16px 0'}}>{product.name}</d4>
                                 {/* Rating */}
-                                <div class="product-detail-rating align-items-center">
+                                <div className="product-detail-rating align-items-center" style={{ marginBottom: '24px' }}>
                                     <img src={`https://res.cloudinary.com/dxsvumas8/image/upload/v1703921413/rating-${Math.round(product.rating)}.png`} alt="" />
                                     <p>13 customer reviews</p>
                                 </div>
                                 {/* Price */}
-                                <p className="product-detail-price">{product.price}</p>
+                                <p className="product-detail-price" style={{ marginBottom: '24px' }}> $ {product.price}</p>
                                 {/* Description */}
-                                <p className="p2">{product.desc}</p>
-                                <div className="d-flex">
-
+                                <p className="p2" style={{  marginBottom: '32px'}}>{product.desc}</p>
+                                <div className="d-flex gap-4">
+                                    <div className="product-quantity">
+                                        <input
+                                            type="button"
+                                            value="-"
+                                            onClick={minusQuantity}
+                                            className="qty-btn"
+                                        />
+                                        <input
+                                            type="text"
+                                            id="quantity"
+                                            name="quantity"
+                                            value={quantity}
+                                            min="1"
+                                            className="qty-input p1"
+                                            readOnly
+                                        />
+                                        <input
+                                            type="button"
+                                            value="+"
+                                            onClick={plusQuantity}
+                                            className="qty-btn"
+                                        />
+                                    </div>
                                     <button className="prim-btn btn-md" style={{ width: '200px' }} onClick={() => { addToCart(product._id) }}>ADD TO CART</button>
                                 </div>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
