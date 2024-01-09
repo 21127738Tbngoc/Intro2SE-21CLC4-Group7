5dae14ad155f05ecd28c835055f16da51f864071
Nguyễn Hữu Khánh <nhkhanh21@clc.fitus.edu.vn>
2024-01-09 13:59:43 +0700

    save cart stage

--- src/duong_frontend/src/components/context/ShopContext.jsx
+++ src/duong_frontend/src/components/context/ShopContext.jsx
@@ -1,72 +1,80 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, {createContext, useEffect, useState} from "react";
 
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
-  const [cartItems, setCartItems] = useState([]);
-  const [allProducts, setAllProducts] = useState([]);
+    let lastStage = JSON.parse(localStorage.getItem("cart"))
+    const [cartItems, setCartItems] = useState(lastStage.cartItems || []);
+    const [allProducts, setAllProducts] = useState(lastStage.allProducts || []);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch("http://localhost:5000/api/products/");
-        const data = await response.json();
-        setAllProducts(data);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch("http://localhost:5000/api/products/");
+                const data = await response.json();
+                setAllProducts(data);
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            }
+        };
+        if (allProducts.length === 0) {
+            fetchProducts();
+        }
+    }, []);
+
+    const addToCart = (itemId) => {
+        setCartItems((prev) => [...prev, itemId]);
     };
 
-    fetchProducts();
-  }, []); 
 
-  const addToCart = (itemId) => {
-    setCartItems((prev) => [...prev, itemId]);
-  };
+    const remove1FromCart = (itemId) => {
+        const indexToRemove = cartItems.indexOf(itemId);
+        if (indexToRemove !== -1) {
+            const updatedCart = [...cartItems];
+            updatedCart.splice(indexToRemove, 1);
+            setCartItems(updatedCart);
+        }
+    };
 
+    const removeallFromCart = (itemId) => {
+        setCartItems((prev) => prev.filter((item) => item !== itemId));
+    };
 
-  const remove1FromCart = (itemId) => {
-    const indexToRemove = cartItems.indexOf(itemId);
-    if (indexToRemove !== -1) {
-      const updatedCart = [...cartItems];
-      updatedCart.splice(indexToRemove, 1);
-      setCartItems(updatedCart);
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const itemId of cartItems) {
+            let itemInfo = allProducts.find((product) => product._id === itemId);
+            totalAmount += itemInfo.price;
+        }
+        return totalAmount;
+    };
+
+    const getTotalCartItems = () => {
+        return cartItems.length;
+    };
+
+    const contextValue = {
+        getTotalCartItems,
+        getTotalCartAmount,
+        allProducts,
+        cartItems,
+        addToCart,
+        removeallFromCart,
+        remove1FromCart,
+    };
+
+    let currentStage = {
+        cartItems: cartItems,
+        allProducts: allProducts
     }
-  };
 
-  const removeallFromCart = (itemId) => {
-    setCartItems((prev) => prev.filter((item) => item !== itemId));
-  };
+    localStorage.setItem("cart", JSON.stringify(currentStage))
+    return (
 
-  const getTotalCartAmount = () => {
-    let totalAmount = 0;
-    for (const itemId of cartItems) {
-      let itemInfo = allProducts.find((product) => product._id === itemId);
-      totalAmount += itemInfo.price;
-    }
-    return totalAmount;
-
-  };
-
-  const getTotalCartItems = () => {
-    return cartItems.length;
-  };
-
-  const contextValue = {
-    getTotalCartItems,
-    getTotalCartAmount,
-    allProducts,
-    cartItems,
-    addToCart,
-    removeallFromCart,
-    remove1FromCart,
-  };
-
-  return (
-    <ShopContext.Provider value={contextValue}>
-      {props.children}
-    </ShopContext.Provider>
-  );
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    );
 };
 
 export default ShopContextProvider;
